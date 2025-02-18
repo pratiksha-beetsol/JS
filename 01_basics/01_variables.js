83361b4c18bfdf7851dfac83c92c1be774ca6f01
pratiksha-beetsol <pratiksha@beetsol.com>
2025-02-18 05:34:22 +0000

    variables and datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=123
+let accountEmail="abc@google.com"
+var accountPassword="1234"
+accountCity="Nashik"
+let accountState;
+//accountId=4
+accountEmail="bbbb@gmail.com"
+accountPassword="111"
+accountCity="pune"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// alert(3+3) // we are using node.js not browser 
+
+
+let name="prati"
+let age=22
+let isLoggedIn=true
+
+console.log(typeof null);
