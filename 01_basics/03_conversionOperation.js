ccde0e6ee8dbdde89163d1ecc1829ca2bba5ca15
pratiksha-beetsol <pratiksha@beetsol.com>
2025-02-18 05:52:03 +0000

    conversion operation done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score=true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score);
+// console.log( valueInNumber);
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=>true; 0=>false ;""=>false
